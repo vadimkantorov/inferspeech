a2765942df864ee8f6b1c458c696b758b9ec66d1
Vadim Kantorov <vadimkantorov@gmail.com>
2019-06-10 14:00:57 +0200

    ...

--- speech2text.py
+++ speech2text.py
@@ -1,3 +1,4 @@
+import sys
 import argparse
 import numpy as np
 import h5py
@@ -6,7 +7,7 @@ import python_speech_features
 import torch
 import torch.nn as nn
 
-def load_model(model_weights, batch_norm_eps = 0.001, dtype = torch.float32):
+def load_model(model_weights, batch_norm_eps = 0.001, backend = torch):
 	def conv_block(kernel_size, num_channels, stride = 1, dilation = 1, repeat = 1, padding = 0):
 		modules = []
 		for i in range(repeat):
@@ -29,8 +30,8 @@ def load_model(model_weights, batch_norm_eps = 0.001, dtype = torch.float32):
 
 	state_dict = {}
 	with h5py.File(model_weights) as h:
-		to_tensor = lambda path: torch.from_numpy(np.asarray(h[path])).to(dtype)
-		for param_name in model.state_dict():
+		to_tensor = lambda path: torch.from_numpy(np.asarray(h[path])).to(torch.float32)
+		for param_name, param in model.state_dict().items():
 			ij = [int(c) for c in param_name if c.isdigit()]
 			if len(ij) > 1:
 				kernel, moving_mean, moving_variance, beta, gamma = [to_tensor(f'ForwardPass/w2l_encoder/conv{1 + ij[0]}{1 + ij[1] // 2}/{suffix}') for suffix in ['kernel', '/bn/moving_mean', '/bn/moving_variance', '/bn/beta', '/bn/gamma']]
@@ -40,34 +41,59 @@ def load_model(model_weights, batch_norm_eps = 0.001, dtype = torch.float32):
 			else:
 				kernel, bias = [to_tensor(f'ForwardPass/fully_connected_ctc_decoder/fully_connected/{suffix}') for suffix in ['kernel', 'bias']]	
 				kernel.unsqueeze_(0)
-			state_dict[param_name] = kernel.permute(2, 1, 0) if 'weight' in param_name else bias
+			state_dict[param_name] = (kernel.permute(2, 1, 0) if 'weight' in param_name else bias).to(param.dtype)
 	model.load_state_dict(state_dict)
+
+	if backend is not torch:
+		def pytorch2keras(module):
+			if isinstance(module, nn.Conv1d):
+				return backend.layers.Conv1D(module.out_channels, module.kernel_size, input_shape = (None, module.in_channels), data_format = 'channels_last', strides = module.stride, dilation_rate = module.dilation, padding = 'same', weights = [module.weight.detach().permute(2, 1, 0).numpy(), module.bias.detach().flatten().numpy()])
+			elif isinstance(module, nn.Sequential):
+				return backend.models.Sequential(list(map(pytorch2keras, module)))
+			elif isinstance(module, nn.Hardtanh):
+				return backend.layers.ReLU(threshold = module.min_val, max_value = module.max_val)
+		model = pytorch2keras(model)
+		model.build((None, None, 64))
 	return model
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-i', '--input_path', default = 'test.wav')
 	parser.add_argument('--weights', default = 'w2l_plus_large_mp.h5')
+	parser.add_argument('--onnx', action = 'store_true')
+	parser.add_argument('--keras', action = 'store_true')
+	parser.add_argument('--tfjs', action = 'store_true')
 	args = parser.parse_args()
 
-	dtype = torch.float32
+	model = load_model(args.weights, backend = torch)
+	model.eval()
+	torch.set_grad_enabled(False)
 
-	sample_rate, signal = scipy.io.wavfile.read(args.input_path)
-	features = torch.from_numpy(python_speech_features.logfbank(signal=signal,
-                            samplerate=sample_rate,
-                            winlen=20e-3,
-                            winstep=10e-3,
-                            nfilt=64,
-                            nfft=512,
-                            lowfreq=0, highfreq=sample_rate/2,
-                            preemph=0.97)).to(dtype)
+	if args.input_path:
+		sample_rate, signal = scipy.io.wavfile.read(args.input_path)
+		features = torch.from_numpy(python_speech_features.logfbank(signal=signal,
+								samplerate=sample_rate,
+								winlen=20e-3,
+								winstep=10e-3,
+								nfilt=64,
+								nfft=512,
+								lowfreq=0, highfreq=sample_rate/2,
+								preemph=0.97)).to(torch.float32)
+		batch = features.t().unsqueeze(0)
+		scores = model(batch).squeeze(0)
 
-	batch = features.t().unsqueeze(0)
-	model = load_model(args.weights, dtype = dtype)
-	scores = model(batch).squeeze(0)
+		decoded_greedy = scores.argmax(dim = 0).tolist()
+		decoded_text = ''.join({0 : ' ', 27 : "'", 28 : '|'}.get(c, chr(c - 1 + ord('a'))) for c in decoded_greedy)
+		postproc_text = ''.join(c for i, c in enumerate(decoded_text) if i == 0 or c != decoded_text[i - 1]).replace('|', '')
+		print(postproc_text)
+
+	if args.onnx:
+		torch.onnx.export(model, batch, args.weights + '.onnx', input_names = ['input'], output_names = ['output'])
 	
-	decoded_greedy = scores.argmax(dim = 0).tolist()
-	decoded_text = ''.join({0 : ' ', 27 : "'", 28 : '|'}.get(c, chr(c - 1 + ord('a'))) for c in decoded_greedy)
-	postproc_text = ''.join(c for i, c in enumerate(decoded_text) if i == 0 or c != decoded_text[i - 1]).replace('|', '')
-
-	print(postproc_text)
+	if args.tfjs:
+		convert_tf_saved_model = None
+		sys.modules['tensorflowjs.converters.tf_saved_model_conversion_v2'] = sys.modules[__name__]
+		import tensorflowjs 
+		import tensorflow.keras
+		model = load_model(args.weights, backend = tensorflow.keras)
+		tensorflowjs.converters.save_keras_model(model, args.weights + '.tfjs')
