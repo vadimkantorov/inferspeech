a092aa0894b0f5b7e1ea7544df242a787fa49a7d
Vadim Kantorov <vadimkantorov@gmail.com>
2019-07-03 11:34:10 +0200

    ...

--- deepdream.py
+++ deepdream.py
@@ -6,63 +6,34 @@ import torch.nn.functional as F
 import speech2text
 
 def get_filterbanks(nfilt, nfft, samplerate):
-	hz2mel = lambda hz: 2595 * math.log10(1+hz/700.);
-	mel2hz = lambda mel: torch.mul(700, torch.sub(torch.pow(10, torch.div(mel, 2595)), 1));
+	hz2mel = lambda hz: 2595 * math.log10(1+hz/700.)
+	mel2hz = lambda mel: torch.mul(700, torch.sub(torch.pow(10, torch.div(mel, 2595)), 1))
 
-	lowfreq = 0;
-	highfreq = samplerate // 2;
-	lowmel = hz2mel(lowfreq);
-	highmel = hz2mel(highfreq);
+	lowfreq = 0
+	highfreq = samplerate // 2
+	lowmel = hz2mel(lowfreq)
+	highmel = hz2mel(highfreq)
 	melpoints = torch.linspace(lowmel,highmel,nfilt+2);
-	bin = torch.floor(torch.mul(nfft+1, torch.div(mel2hz(melpoints), samplerate))).tolist();
+	bin = torch.floor(torch.mul(nfft+1, torch.div(mel2hz(melpoints), samplerate))).tolist()
 
-	fbank = torch.zeros([nfilt, nfft // 2 + 1]).tolist();
+	fbank = torch.zeros([nfilt, nfft // 2 + 1]).tolist()
 	for j in range(nfilt):
 		for i in range(int(bin[j]), int(bin[j+1])):
 			fbank[j][i] = (i - bin[j]) / (bin[j+1]-bin[j])
 		for i in range(int(bin[j+1]), int(bin[j+2])):
 			fbank[j][i] = (bin[j+2]-i) / (bin[j+2]-bin[j+1])
-	return torch.tensor(fbank);
+	return torch.tensor(fbank)
 
 def frontend(sample_rate, signal, nfft = 512, nfilt = 64, preemph = 0.97):
-	def frame(signal, frameLength, frameStep):
-		start = 0;
-		output = [];
-		while (start + frameLength <= signal.numel()):
-			output.append(signal[start:start + frameLength])
-			start += frameStep
-		return torch.stack(output)
-
-	def stft_abs_sq(signal, nfft, frameLength, frameStep):
-		fftLength = nfft;
-		framedSignal = frame(signal, frameLength, frameStep);
-		framedSignal = torch.cat([framedSignal, torch.zeros(len(framedSignal), fftLength - framedSignal.shape[1])], dim = -1)
-		windowedSignal = torch.mul(framedSignal, torch.hann_window(nfft));
-
-		torch_abs = lambda x: (x ** 2).sum(dim = -1).sqrt()
-		output = [];
-		for i in range(framedSignal.shape[0]):
-			output.append(torch_abs(torch.rfft(windowedSignal[i, :fftLength], 1)) ** 2);
-		return torch.div(torch.stack(output), nfft);
-
-	def preemphasis(signal, coeff):
-		return torch.cat([signal[:1], torch.sub(signal[1:], torch.mul(signal[:-1], coeff))]);
+	preemphasis = lambda signal, coeff: torch.cat([signal[:1], torch.sub(signal[1:], torch.mul(signal[:-1], coeff))])
 	
-	window_length = 20 * sample_rate // 1000;
-	hop_length = 10 * sample_rate // 1000;
-
-	signal = preemphasis(signal, preemph);
-	#pspec = stft_abs_sq(signal, nfft, window_length, hop_length);
+	window_length = 20 * sample_rate // 1000
+	hop_length = 10 * sample_rate // 1000
+	signal = preemphasis(signal, preemph)
 	pspec = torch.stft(signal, nfft, win_length = window_length, hop_length = hop_length, window = torch.hann_window(window_length), pad_mode = 'constant', center = False).pow(2).sum(dim = -1).t() / nfft
-	mel_basis = get_filterbanks(nfilt, nfft, sample_rate).t();
-	features = torch.log(torch.add(torch.matmul(pspec, mel_basis), 1e-20));
-
-	batch = features.t()
-	m = batch.mean()
-	s = batch.std()
-	batch = (batch - m) / s
-
-	return batch
+	mel_basis = get_filterbanks(nfilt, nfft, sample_rate).t()
+	features = torch.log(torch.add(torch.matmul(pspec, mel_basis), 1e-20))
+	return (features.t() - features.mean()) / features.std()
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
@@ -77,23 +48,22 @@ if __name__ == '__main__':
 
 	model = speech2text.load_model(args.weights).to(args.device)
 	sample_rate, signal = scipy.io.wavfile.read(args.input_path)
-	signal = torch.from_numpy(signal).to(torch.float32).requires_grad_()
-	labels = torch.IntTensor([{' ' : 0, "'" : 27}.get(c, 1 + ord(c) - ord('A')) for c in args.transcript])
+	signal = torch.from_numpy(signal).to(torch.float32).to(args.device).requires_grad_()
+	labels = torch.IntTensor([{' ' : 0, "'" : 27}.get(c, 1 + ord(c) - ord('A')) for c in args.transcript]).to(args.device)
 
 	print('R', args.transcript)
 	for i in range(args.num_iter):
 		batch = frontend(sample_rate, signal).unsqueeze(0)
-		scores = model(batch)
+		scores = model(batch).requires_grad_()
 
 		decoded_greedy = scores.squeeze(0).argmax(dim = 0).tolist()
 		decoded_text = ''.join({0 : ' ', 27 : "'", 28 : '|'}.get(c, chr(c - 1 + ord('A'))) for c in decoded_greedy)
 		print(i, ''.join(c for i, c in enumerate(decoded_text) if i == 0 or c != decoded_text[i - 1]).replace('|', ''))
-		loss = torch.nn.functional.ctc_loss(F.log_softmax(scores, dim = 1).permute(2, 0, 1), labels.unsqueeze(0), torch.IntTensor([scores.shape[-1]]), torch.IntTensor([len(labels)]))
+		loss = F.ctc_loss(F.log_softmax(scores, dim = 1).permute(2, 0, 1), labels.unsqueeze(0), torch.IntTensor([scores.shape[-1]]).to(args.device), torch.IntTensor([len(labels)]).to(args.device))
 		model.zero_grad()
 		loss.backward()
-		signal.grad.data[torch.isnan(signal.grad.data)] = 0
 		signal.data.sub_(signal.grad.data.mul_(args.lr))
 		signal.grad.data.zero_()
 		print(i, float(loss))
 
-	scipy.io.wavfile.write(args.output_path, sample_rate, signal.to(torch.int16).numpy())
+	scipy.io.wavfile.write(args.output_path, sample_rate, signal.detach().cpu().to(torch.int16).numpy())
